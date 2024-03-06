f6566e8c8808b8f1bf5e5ae77e34bfed4679e58a
Tyler Li <sh0risu179@outlook.com>
2024-03-06 13:45:40 +0900

    schema changes

--- internal/db/vtuberSchema.sql
+++ internal/db/vtuberSchema.sql
@@ -1,28 +1,35 @@
 CREATE TABLE Vtubers (
-	"uuid" BIGSERIAL AUTO_INCREMENT,
+	"vtubers_id" BIGSERIAL AUTO_INCREMENT,
 	"groups_id" SERIAL,
 	"name_default" VARCHAR NOT NULL,
 	"name_en" VARCHAR,
 	"name_jp" VARCHAR,
 	"name_cn" VARCHAR,
 	"bio" TEXT,
---	"platforms" VARCHAR NOT NULL, 
-	"youtube_link" VARCHAR NOT NULL,
 	"languages" CHAR(2) NOT NULL,
 	"debut_date" DATE, 
-	PRIMARY KEY ("uuid"),
-	FOREIGN KEY ("groups_id") REFERENCES Groups("id")
+	PRIMARY KEY ("vtubers_id"),
+	FOREIGN KEY ("groups_id") REFERENCES Groups("groups_id")
 );
 
 -- TODO Relate platforms to accounts instead of just youtube
--- CREATE TABLE "platforms" (
-	-- "id" SERIAL PRIMARY KEY,
-	-- "link_syntax" VARCHAR NOT NULL,
-	-- "uri_template" VARCHAR NOT NULL,
--- );
+ CREATE TABLE Platforms (
+	"platforms_id" SERIAL PRIMARY KEY,
+	"uri_template" VARCHAR NOT NULL,
+);
 
 CREATE TABLE Groups (
-	"id" SERIAL PRIMARY KEY,
+	"groups_id" SERIAL PRIMARY KEY,
 	"name" VARCHAR(30) NOT NULL,
 	"website" VARCHAR,
+);
+
+-- Relations
+CREATE TABLE streamOn (
+	"account_id" BIGSERIAL NOT NULL,
+	"vtubers_id" BIGSERIAL NOT NULL,
+	"platforms_id" SERIAL NOT NULL,
+	PRIMARY KEY(vtubers_id, platforms_id),
+	FOREIGN KEY(vtubers_id) REFERENCES Vtubers(vtubers_id),
+	FOREIGN KEY(platforms_id) REFERENCES Platforms(platforms_id)
 );
